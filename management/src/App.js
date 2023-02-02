7db165bc32cde26a09240e7140cad7bb2ad2e483
hackchankim <81866286+hackchankim@users.noreply.github.com>
2023-02-02 20:11:29 +0900

    Create React Project

--- management/src/App.js
+++ management/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="gray-background">
-      <img src={logo} lat="logo"/>
+      <img src={logo} lat="logo"/> 
       <h2> Let's develop managemnt system! </h2>
     </div>
   );
