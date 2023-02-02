15aff32df3311b9c69e23d9d17688307f8eb2c9f
hackchankim <81866286+hackchankim@users.noreply.github.com>
2023-02-02 20:42:20 +0900

    lecture 4

--- management/src/App.js
+++ management/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'name':'티모시',
+  'birthday':'210104',
+  'gender':'남자',
+  'job':'회사원'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/> 
-      <h2> Let's develop managemnt system! </h2>
-    </div>
+    <Customer 
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
