1fdfb55fe192c0be362178132f1f81f9f1183d48
hackchankim <81866286+hackchankim@users.noreply.github.com>
2023-02-10 16:25:04 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -2,21 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
   'name':'티모시',
   'birthday':'210104',
   'gender':'남자',
   'job':'회사원'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'210104',
+  'gender':'남자',
+  'job':'회사원'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'김티모',
+  'birthday':'210104',
+  'gender':'남자',
+  'job':'회사원'
 }
+]
 
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map( c=> {
+          return <Customer 
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}/>
+        })
+      }
+    </div>
   );
 }
 
